f648385d3233f499c26e64de4ea1f528088d6d89
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-21 17:31:50 -0500

    function written and psuedocode written. passing

--- week-1/find-first-duplicate/ruby/find_first_duplicate.rb
+++ week-1/find-first-duplicate/ruby/find_first_duplicate.rb
@@ -1,5 +1,14 @@
 def find_first_duplicate(arr)
-  # type your code in here
+  new_array = []
+
+  arr.each do |x|
+    new_array.push(x)
+    if new_array.count(x) > 1 then
+      return x
+    end
+  end
+
+  return -1
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -16,3 +25,9 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# want to use "each" array method
+# save the variable to a new array and check the new array for a count that is higher than 1
+# return value if count is greater than 1
+# if a value is not found that is greater than one, then at end of function, return -1
+# the -1 will never be reached if a duplicate is found
