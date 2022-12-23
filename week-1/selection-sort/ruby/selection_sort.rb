b0575b457112dca890f8a12c58c6cc2f08e150ff
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-22 23:29:38 -0500

    improved way to sort

--- week-1/selection-sort/ruby/selection_sort.rb
+++ week-1/selection-sort/ruby/selection_sort.rb
@@ -2,18 +2,13 @@ def selection_sort(arr)
   # [ 2, 1, -1, 3, 6, 4]
   # [ -1, 1, 2, 3, 4, 6]
 
-  i = 0
-  while ((i != arr.size - 1) && (arr.size > 0))
-   if arr[i] > arr[i + 1]
-    arr[i], arr[i + 1] = arr[i + 1], arr[i]
-    i = 0
-   else
-    i = i + 1
-   end
+  arr_sorted = []
+  while arr.size > 0
+    arr_sorted << arr.min
+    arr.delete_at(arr.index(arr.min))
   end
 
-  return arr
-
+  return arr_sorted
 
 end
 
@@ -35,7 +30,30 @@ end
 # Please add your pseudocode to this file
 # And a written explanation of your solution
 
+##2nd attempt
+# find minimum, add to new array, delete from existing array. Continue until existing array
+#no longer has any elements in it.
+
+## first attempt
 # need to sort an array from smallest to largest
 # compare a number to the number to the left of it.
 # if number is smaller, move it, and subtract 1 from index
 # continue this loop until number is no longer smaller
+
+# def selection_sort(arr)
+#   # [ 2, 1, -1, 3, 6, 4]
+#   # [ -1, 1, 2, 3, 4, 6]
+
+#   i = 0
+#   while ((i != arr.size - 1) && (arr.size > 0))
+#    if arr[i] > arr[i + 1]
+#     arr[i], arr[i + 1] = arr[i + 1], arr[i]
+#     i = 0
+#    else
+#     i = i + 1
+#    end
+#   end
+
+#   return arr
+
+# end
