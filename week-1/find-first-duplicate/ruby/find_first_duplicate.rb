d52283ae886f733dbb74e1d6b16ff49e13a316f0
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-22 22:13:47 -0500

    removed comments and code to get back to original

--- week-1/find-first-duplicate/ruby/find_first_duplicate.rb
+++ week-1/find-first-duplicate/ruby/find_first_duplicate.rb
@@ -1,14 +1,5 @@
 def find_first_duplicate(arr)
-  new_array = []
-
-  arr.each do |x|
-    new_array.push(x)
-    if new_array.count(x) > 1 then
-      return x
-    end
-  end
-
-  return -1
+ # code here
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -25,9 +16,3 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
-
-# want to use "each" array method
-# save the variable to a new array and check the new array for a count that is higher than 1
-# return value if count is greater than 1
-# if a value is not found that is greater than one, then at end of function, return -1
-# the -1 will never be reached if a duplicate is found
--- week-1/reverse-string/ruby/reverse_string.rb
+++ week-1/reverse-string/ruby/reverse_string.rb
@@ -1,12 +1,5 @@
-require "benchmark"
-
 def reverse_string(str)
-  new_string = ""
-  str.chars.each do |char|
-    new_string = char + new_string
-  end
-  return new_string
-  
+  # code here
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -23,33 +16,3 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
-
-# looking for "him" to switch to "mih"
-  # need to break "him" into an array for each character
-  # array = [h, i, m]
-  # we want:
-  # array[0] = newarray[2]
-  # array[1] = newarray[1]
-  # array[2] = newarray[0]
-
-
-# first attempt. according to rspec on my computer, took 0.0027 seconds to finish (files took .07 seconds to load)
-# def reverse_string(str)
-#   orig_str_array = str.split("")
-#   new_str_array = []
-#   end_length = orig_str_array.length - 1
-#   for i in 0..end_length do
-#     pointer = end_length - i
-#     new_str_array.push(orig_str_array[pointer])
-#   end
-#   new_string = new_str_array.join("")
-#   return new_string
-
-# end
-
-# second attempt:
-# create new variable
-# cycle through each character of the string and add it to the beginning of the new_string
-# variable.
-
-# did some speed testing and second attempt was much faster
