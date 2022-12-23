2c3c757303842a6191ca222d5a2547050b1b01d0
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-22 22:20:03 -0500

    wrote out psuedocode notes

--- week-1/fibonacci-series/ruby/fibonacci.rb
+++ week-1/fibonacci-series/ruby/fibonacci.rb
@@ -1,5 +1,5 @@
 def fibonacci(num)
-  # type your code in here
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -23,3 +23,10 @@ end
 # And a written explanation of your solution
 
 #test for github
+
+# fibonacci series is 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+# add the two previous to get the next number
+# using 0 index as the first number, we want to be able to enter a value and return
+# that index within the sequence.
+# to do that we need to perform the fibonacci sequence up to that index by adding 2 previous values
+
