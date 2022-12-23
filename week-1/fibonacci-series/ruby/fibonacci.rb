8d35c4308533f42def5ac7b25eed5275f169600b
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-22 22:39:18 -0500

    created code for fibonacci series

--- week-1/fibonacci-series/ruby/fibonacci.rb
+++ week-1/fibonacci-series/ruby/fibonacci.rb
@@ -1,5 +1,17 @@
-def fibonacci(num)
+require 'pry'
 
+def fibonacci(num)
+ # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+ fib_series = [0, 1]
+
+ for i in 0..num do
+  if fib_series[i] == nil
+    fib_num = fib_series[i - 1] + fib_series[i - 2]
+    fib_series.push(fib_num)
+  end
+ end
+
+ return fib_series[num]
 end
 
 if __FILE__ == $PROGRAM_NAME
