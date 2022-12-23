6a1d4c3fb310a3139a1f98ce4bbdeb0300b776bb
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-22 23:22:30 -0500

    coded solution

--- week-1/selection-sort/ruby/selection_sort.rb
+++ week-1/selection-sort/ruby/selection_sort.rb
@@ -1,5 +1,20 @@
 def selection_sort(arr)
-  # type your code in here
+  # [ 2, 1, -1, 3, 6, 4]
+  # [ -1, 1, 2, 3, 4, 6]
+
+  i = 0
+  while ((i != arr.size - 1) && (arr.size > 0))
+   if arr[i] > arr[i + 1]
+    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    i = 0
+   else
+    i = i + 1
+   end
+  end
+
+  return arr
+
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -19,3 +34,8 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# need to sort an array from smallest to largest
+# compare a number to the number to the left of it.
+# if number is smaller, move it, and subtract 1 from index
+# continue this loop until number is no longer smaller
