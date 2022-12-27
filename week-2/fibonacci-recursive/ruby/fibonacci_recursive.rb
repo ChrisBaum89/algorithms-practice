fa2bb5d34cf4d96856e44efc11e7beab61d0b2d5
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-26 19:06:02 -0500

    updated recursion code

--- week-2/fibonacci-recursive/ruby/fibonacci_recursive.rb
+++ week-2/fibonacci-recursive/ruby/fibonacci_recursive.rb
@@ -1,5 +1,10 @@
 def fibonacci(n)
-  # type your code in here
+  # 0, 1, 1, 2, 3, 5, 8, 13, 21
+  if n < 2
+    return n
+  else
+    fibonacci(n - 1) + fibonacci(n - 2)
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
