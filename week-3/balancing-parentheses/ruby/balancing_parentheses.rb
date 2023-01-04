7e691a5124233f0d6e49f561b9f52b5ff9be169c
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-03 23:06:35 -0500

    updated logic for finding open parentheses

--- week-3/balancing-parentheses/ruby/balancing_parentheses.rb
+++ week-3/balancing-parentheses/ruby/balancing_parentheses.rb
@@ -1,5 +1,20 @@
 def balancing_parentheses(string)
-  
+ open_par = 0
+ missing_par = 0
+
+ string.chars.each do |char|
+  if char == "("
+    open_par += 1
+    next
+  end
+  if open_par > 0
+    open_par -= 1
+  else
+    missing_par += 1
+  end
+ end
+
+  return (open_par + missing_par)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -20,6 +35,12 @@ if __FILE__ == $PROGRAM_NAME
 
   puts "Expecting: 2"
   puts balancing_parentheses(')(')
+
+  puts
+
+  puts "Expecting: 3"
+  puts balancing_parentheses('(((')
+
 
   # Don't forget to add your own!
 end
