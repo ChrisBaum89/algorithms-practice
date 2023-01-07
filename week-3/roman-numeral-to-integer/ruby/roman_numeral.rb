e85c00f3a12ff7a2b9a9cf25ad1793074c5bec80
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-06 23:46:53 -0500

    logic for calculating roman numeral

--- week-3/roman-numeral-to-integer/ruby/roman_numeral.rb
+++ week-3/roman-numeral-to-integer/ruby/roman_numeral.rb
@@ -1,20 +1,61 @@
+
+
 def roman_numeral(string)
-  # type your code in here
+  #call symbols e.g. symbols[:II]
+  #need unique 
+  symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+
+  sum = 0
+  idx_max = string.length - 1
+
+  for i in 0..idx_max do
+    value = symbols[string[i].to_sym]
+    sum = sum + value
+    if smaller_number_check?(string, symbols, i)
+      sum = sum - 2 * symbols[string[i].to_sym]
+    end
+  end
+
+  sum
+
+end
+
+def smaller_number_check?(string, symbols, i)
+  value_1 = symbols[string[i].to_sym]
+  value_2 = calc_value_2(string, symbols, i)
+  value_1 < value_2 ? true : false
+end
+
+def calc_value_2 (string, symbols, i)
+  string[i+1] != nil ? symbols[string[i+1].to_sym] : 0
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Expecting: 1"
-  puts roman_numeral('I')
+  # puts "Expecting: 1"
+  # puts roman_numeral('I')
 
-  puts
+  # puts
 
-  puts "Expecting: 9"
-  puts roman_numeral('IX')
+  # puts "Expecting: 9"
+  # puts roman_numeral('IX')
 
-  puts
+  # puts
 
   puts "Expecting: 402"
   puts roman_numeral('CDII')
+
+  puts
+
+  puts "Expecting: 3849"
+  puts roman_numeral('MMMDCCCXLIX')
 
   # Don't forget to add your own!
 end
