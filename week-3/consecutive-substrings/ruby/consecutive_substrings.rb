e09be9d57ad066cad971e1cf4b6c0d1758bfb922
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-06 20:54:37 -0500

    logic for solution to algorithm

--- week-3/consecutive-substrings/ruby/consecutive_substrings.rb
+++ week-3/consecutive-substrings/ruby/consecutive_substrings.rb
@@ -1,6 +1,16 @@
 def consecutive_substrings(string)
-  arr = string.split('')
-  
+# string[0..0], string[0..1], string [0..2], string [0..3], string [1..1]
+# string [1..2], string[1..3], string [2..2]
+
+  arr = []
+  index_max = string.length - 1
+  for i in 0..index_max do
+    for j in i..index_max do
+      arr.push(string[i..j])
+    end
+   
+  end
+  arr
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -13,6 +23,12 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: ['a']"
   print "=> " 
   print consecutive_substrings('a')
+
+  puts
+
+  puts "Expecting: ['a, ab, abc, abcd, b, bc, bcd, c, cd, d']"
+  print "=> " 
+  print consecutive_substrings('abcd')
 
   # Don't forget to add your own!
 end
